2307b5b8c1e1314eda75fc1f29f7d78ba95c0938
Romain Beaumont <romain.rom1@gmail.com>
2016-04-11 23:40:49 +0200

    add saving example

--- examples/anvil.js
+++ examples/anvil.js
@@ -2,7 +2,18 @@
 var Anvil = require('../index').Anvil;
 
 const anvil = new Anvil('../world/db');
-anvil.loadRaw(2, 4, 0x30).then(data => {
-  console.log(data);
-  anvil.close();
-});
+anvil
+  .loadRaw(2, 4, 0x30)
+  .then(data => {
+    console.log(data);
+    return data;
+  })
+  .then(data => {
+    data[0]=5;
+    return anvil.saveRaw(2, 4, 0x30, data);
+  })
+  .then(() => anvil.loadRaw(2,4,0x30))
+  .then(data => {
+    console.log(data);
+    anvil.close();
+  });
