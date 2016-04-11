911f7ca46ad6ae9329c6aeaf89fc9773573c6afa
Romain Beaumont <romain.rom1@gmail.com>
2016-04-11 23:37:35 +0200

    close after you're done, not before

--- examples/anvil.js
+++ examples/anvil.js
@@ -2,5 +2,7 @@
 var Anvil = require('../index').Anvil;
 
 const anvil = new Anvil('../world/db');
-anvil.loadRaw(2, 4, 0x30).then(data => console.log(data));
-anvil.close();
+anvil.loadRaw(2, 4, 0x30).then(data => {
+  console.log(data);
+  anvil.close();
+});
